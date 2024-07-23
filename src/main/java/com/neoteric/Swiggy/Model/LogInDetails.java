4f935b63872ea5738abd33322667a38a2079b488
neoteric <asampathkumar545@gmail.com>
2024-07-23 16:09:00 +0530

    login and list of restaurants

--- src/main/java/com/neoteric/Swiggy/Model/LogInDetails.java
+++ src/main/java/com/neoteric/Swiggy/Model/LogInDetails.java
@@ -1,11 +1,14 @@
 package com.neoteric.Swiggy.Model;
 
 public class LogInDetails {
+    public String userName;
+    public String password;
 
-
-
-
-
-
-
+    @Override
+    public String toString() {
+        return "LogInDetails{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
